002ca32ee943435c3df62b94b55b5e6c094f3127
alperennbulutt <alperenbulut.ab@gmail.com>
2023-07-16 01:49:47 +0300

    structure create, design part missing

---
+++ lib/constant/constants.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+const kAppName = 'SpaceX Latest Launch';
+const kNetworkProblemText = 'Network Problem!';
+const kDetailsText = 'Details';
+const kFlightNumberText = 'Flight Number: ';
+const kDateText = 'Date: ';
+const kArticleText = 'Article: ';
+const kWikipediaText = 'Wikipedia: ';
+const kYoutubeText = 'Youtube: ';
+const kSpace = 10.0;
+const kDoubleSpace = 20.0;
+const kPadding = EdgeInsets.all(10.0);
+const kCardBackgroundColor = Color(0xff1B1B1B);
+
+const kBackgroundColor = Color.fromARGB(255, 55, 54, 54);
+
+const kTitleTextStyle = TextStyle(fontSize: 22, color: Colors.white);
+const kDetailBodyTextStyle = TextStyle(
+  fontSize: 18,
+  color: Colors.white70,
+);
+const kDetailBodyImportantInfoTextStyle = TextStyle(
+  fontSize: 18,
+  color: Colors.white,
+);
+const kDetailBodyLinkTextStyle = TextStyle(
+  fontSize: 18,
+  color: Colors.lightBlue,
+  decoration: TextDecoration.underline,
+);
--- lib/spacex/views/space_x_scren.dart
+++ lib/spacex/views/space_x_scren.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spacex_latest_launch/constant/constants.dart';
 import 'package:spacex_latest_launch/spacex/bloc/spacex_bloc.dart';
 
 class SpacexScreen extends StatelessWidget {
@@ -8,7 +9,11 @@ class SpacexScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
+      backgroundColor: kBackgroundColor,
       appBar: AppBar(
         title: Text("Spacex Screen"),
       ),
@@ -23,32 +28,50 @@ class SpacexScreen extends StatelessWidget {
             );
           }
           if (state is SpacexLoaded) {
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Text(
-                    state.spacexModel.name!,
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Card(
+                  color: kCardBackgroundColor,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Text(
+                        state.spacexModel.name!,
+                        style: kTitleTextStyle,
+                      ),
+                      trailing: state.spacexModel.links!.patch!.small != null
+                          ? Image.network(
+                              state.spacexModel.links!.patch!.small!)
+                          : const CircularProgressIndicator(),
+                    ),
                   ),
-                  // Text(
-                  //   state.spacexModel.type,
-                  // ),
-                  // Text(
-                  //   state.spacexModel.participants.toString(),
-                  // ),
-                  // ElevatedButton(
-                  //   onPressed: () {
-                  //     context.read<BoredBloc>().add(LoadApiEvent());
-                  //   },
-                  //   child: Text("Load New Activity"),
-                  // ),
-                ],
-              ),
+                ),
+
+                SizedBox(
+                  height: height * 0.01,
+                ),
+
+                // url luncher ve sliver eklenecek!!
+
+                Card(
+                    color: kCardBackgroundColor,
+                    child: Column(
+                      children: [
+                        Text(
+                          kWikipediaText + state.spacexModel.links!.wikipedia!,
+                          style: kTitleTextStyle,
+                        ),
+                      ],
+                    )),
+
+                Spacer(),
+              ],
             );
           }
-          return Text("Failed");
+          return Text("Something went wrong");
         },
       ),
     );
